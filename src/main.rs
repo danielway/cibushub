fbec09ccaf297ac2dd6c42cfae56fca3ae2d9975
Daniel Way <projects@danieldway.com>
2023-02-17 20:08:22 -0600

    feat(mainmenu): add screen selection enum

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use std::{io::stdout, thread::sleep, time::Duration};
+use std::io::stdout;
 
 use crossterm::event::{read, Event, KeyCode};
 use tty_interface::{Interface, Result, Position, pos};
@@ -11,16 +11,21 @@ fn execute() -> Result<()> {
     let mut stdout = stdout();
     let mut interface = Interface::new_alternate(&mut stdout)?;
 
-    main_menu(&mut interface)?;
-    sleep(Duration::from_millis(2000));
+    loop {
+        if let Some(_screen) = main_menu(&mut interface)? {
+            // TODO
+        } else {
+            break;
+        }
+    }
 
     interface.exit()?;
 
     Ok(())
 }
 
-fn main_menu(interface: &mut Interface) -> Result<()> {
-    let options = ["Meal Plan", "Grocery", "Recipes", "Pantry"];
+fn main_menu(interface: &mut Interface) -> Result<Option<Screen>> {
+    let options = Screen::all();
     let mut selected = 0;
     loop {
         interface.clear_rest_of_interface(pos!(0, 0));
@@ -31,7 +36,7 @@ fn main_menu(interface: &mut Interface) -> Result<()> {
                 interface.set(pos!(2, item as u16 + 2), ">");
             }
 
-            interface.set(pos!(4, item as u16 + 2), option);
+            interface.set(pos!(4, item as u16 + 2), &option.get_text());
         }
     
         interface.apply()?;
@@ -55,11 +60,31 @@ fn main_menu(interface: &mut Interface) -> Result<()> {
                         }
                         break;
                     }
-                    KeyCode::Esc => return Ok(()),
+                    KeyCode::Enter => return Ok(Some(options[selected])),
+                    KeyCode::Esc => return Ok(None),
                     _ => {}
                 }
                 _ => {}
             }
         }
+    }
+}
+
+#[derive(Copy, Clone)]
+enum Screen { MainMenu, MealPlan, Grocery, Recipes, Pantry }
+
+impl Screen {
+    fn get_text(self) -> String {
+        match self {
+            Screen::MainMenu => "Main Menu",
+            Screen::MealPlan => "Meal Plan",
+            Screen::Grocery => "Grocery",
+            Screen::Recipes => "Recipes",
+            Screen::Pantry => "Pantry"
+        }.to_string()
+    }
+
+    fn all() -> [Screen; 5] {
+        [Screen::MainMenu, Screen::MealPlan, Screen::Grocery, Screen::Recipes, Screen::Pantry]
     }
 }
