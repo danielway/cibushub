625587aaeb4ee5709e3d6e17b9fed16698f93816
Daniel Way <projects@danieldway.com>
2023-02-17 19:32:51 -0600

    feat: add "hello world" with tty-interface

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+use std::{io::stdout, thread::sleep, time::Duration};
+
+use tty_interface::{Interface, Result, Position, pos};
+
 fn main() {
-    println!("Hello, world!");
+    execute().unwrap()
+}
+
+fn execute() -> Result<()> {
+    let mut stdout = stdout();
+    let mut interface = Interface::new_alternate(&mut stdout)?;
+
+    interface.set(pos!(0, 0), "Hello, world!");
+    interface.apply()?;
+
+    sleep(Duration::from_millis(2000));
+
+    interface.exit()?;
+
+    Ok(())
 }
